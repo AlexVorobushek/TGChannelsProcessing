a031561f205bc0c6ce7f1892e5d2a29be41019a9
AlexVorobushek <alex080605@mail.ru>
2023-04-29 01:28:59 +0300

    fixed getting message text

--- TGChannelsParser.py
+++ TGChannelsParser.py
@@ -1,16 +1,28 @@
 from pyrogram import Client
 from Settings import Settings
+from progress.bar import Bar
 
 
 class TGChannelsParser:
     @staticmethod
+    def getTextFromMessage(message):
+        if message.text:
+            return message.text
+        elif message.caption:
+            return message.caption
+        elif message.poll:
+            return message.poll.question + " " + " ".join([answer.text for answer in message.poll.options])
+    @staticmethod
     async def parse(channels):
         tracked_messages = dict()
         async with Client("my_account", Settings.ChannelsParser.api_id, Settings.ChannelsParser.api_hash) as app:
+            bar = Bar('Channels', max=len(channels), fill="*")
             for channel in channels:
                 if await app.get_chat_members_count(channel) < Settings.ChannelsFilter.min_members_to_track: continue
                 async for message in app.get_chat_history(channel, offset_date=Settings.ChannelsParser.date_to):
                     if message.date < Settings.ChannelsParser.date_from: break
-                    if not message.text: continue
-                    tracked_messages[channel] = tracked_messages.get(channel, []) + [message.text]
+                    if text:=TGChannelsParser.getTextFromMessage(message):
+                        tracked_messages[channel] = tracked_messages.get(channel, []) + [text]
+                bar.next()
+            bar.finish()
         return tracked_messages
